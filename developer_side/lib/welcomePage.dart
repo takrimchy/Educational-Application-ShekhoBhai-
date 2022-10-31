8e588debb355b9693408524b71315caaf897b15e
tajbidtousif <tajbidtousif@gmail.com>
2022-10-31 18:24:02 +0600

    Developing...

--- developer_side/lib/welcomePage.dart
+++ developer_side/lib/welcomePage.dart
@@ -10,90 +10,145 @@ class welcomePage extends StatelessWidget {
     var height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: LiquidSwipe(
-
-        enableLoop: true,
-        enableSideReveal: true,
-        slideIconWidget: const Icon(Icons.arrow_back_ios),
-        positionSlideIcon: 0.5,
-
-        pages: [
-
-          Container(
-          color: Colors.red,
-          child: const Center(child: Text("First"),),
-          ),
-
-          Container(
-            color: Colors.yellowAccent,
-            child: const Center(child: Text("Second"),),
-          ),
-
-          Container(
-            color: Colors.white,
-            padding:  EdgeInsets.all(35.0),
-            child: Column(
+        body: LiquidSwipe(
+      enableLoop: false,
+      enableSideReveal: true,
+      slideIconWidget: const Icon(Icons.arrow_back_ios),
+      positionSlideIcon: 0.5,
+      pages: [
+        Container(
+          color: Colors.deepPurpleAccent,
+          padding: EdgeInsets.all(30.0),
+          child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Image(
-                  image:  AssetImage('assets/welcomeimage.jpg'),
+                  image: AssetImage('assets/asAStudent.png'),
                   height: height * 0.3,
                 ),
                 Column(
-                  children: [
+                  children: const [
                     Text(
-                      "Welcome to ShekhoBha!",
-                      style: Theme.of(context).textTheme.headline4,
+                      "Student Feature",
+                      style:
+                        TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        )
+                     // Theme.of(context).textTheme.headline4,
+
                     ),
-                    const SizedBox(
+                    SizedBox(
                       height: 10,
                     ),
-                    const Text(
-                      "We create a bridge between learners and mentors",
+                    Text(
+                      "* Complete Guideline About Carrier Choice \n * Complete Guideline of Chosen Carrier\n*Could Ask Question to His Favourite Professor",
                       style: TextStyle(fontSize: 15),
                     ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: OutlinedButton(
-                          onPressed: () {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) => lonIn()));
-                          },
-                          style: OutlinedButton.styleFrom(
-                              shape: const RoundedRectangleBorder(),
-                              side: const BorderSide(color: Colors.black),
-                              padding: const EdgeInsets.symmetric(vertical: 8)),
-                          child: const Text(
-                            "LOGIN",
-                            style: TextStyle(color: Colors.black),
-                          )),
+              ]),
+        ),
+        Container(
+          color: Colors.tealAccent,
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image(
+                  image: const AssetImage('assets/asATeacher.png'),
+                  height: height * 0.3,
+                ),
+                Column(
+                  children: const [
+                    Text(
+                        "Teacher Feature",
+                        style:
+                        TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        )
+                      // Theme.of(context).textTheme.headline4,
+
                     ),
-                    const SizedBox(
-                      width: 10,
+                    SizedBox(
+                      height: 10,
                     ),
-                    Expanded(
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            side: const BorderSide(color: Colors.blueAccent),
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                          ),
-                          child: const Text("SIGNUP")),
+                    Text(
+                      "* The best teachers are those who show us where to look but don't tell us what to see ",
+                      style: TextStyle(fontSize: 15),
                     ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ]),
+        ),
+
+
+
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(35.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image(
+                image: AssetImage('assets/welcomeimage.jpg'),
+                height: height * 0.3,
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Welcome to ShekhoBha!",
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "We create a bridge between learners and mentors",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => lonIn()));
+                        },
+                        style: OutlinedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(),
+                            side: const BorderSide(color: Colors.black),
+                            padding: const EdgeInsets.symmetric(vertical: 8)),
+                        child: const Text(
+                          "LOGIN",
+                          style: TextStyle(color: Colors.black),
+                        )),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          side: const BorderSide(color: Colors.blueAccent),
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                        ),
+                        child: const Text("SIGNUP")),
+                  ),
+                ],
+              )
+            ],
           ),
+        ),
 
 
-        ],
-
-      )
-    );
-
+      ],
+    ));
   }
 }
