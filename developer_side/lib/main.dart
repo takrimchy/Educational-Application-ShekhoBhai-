fc1df75004eed8da71b368a3ac5e157f320f94f2
tajbidtousif <tajbidtousif@gmail.com>
2022-11-07 20:48:32 +0600

    developing

--- developer_side/lib/main.dart
+++ developer_side/lib/main.dart
@@ -3,22 +3,23 @@ import 'logIn.dart';
 import 'signIn.dart';
 import 'welcomePage.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: lonIn(),
-
-    ); // MaterialApp
+      home: welcomePage(),
+    );
   }
-
 }
